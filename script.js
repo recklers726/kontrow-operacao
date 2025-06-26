07e8b8021a6a0c715d8f6577e3854d64008b06ba
recklers726 <carretero909@gmail.com>
2025-06-26 14:24:02 -0300

    atualizacao 3.8

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ botao.addventListener('mouseenter', () => {
 bota.addventListener('mouseleave', () => {
     menu.ClassList.remove('zoom-ativo');
 });
+
+  const btnTopo = document.getElementById("btn-topo");
+
+  // Mostrar o botão quando o usuário rolar a página
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btnTopo.style.display = "block";
+    } else {
+      btnTopo.style.display = "none";
+    }
+  };
+
+  
+
+  // Ao clicar no botão, rolar suavemente para o topo
+  btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
